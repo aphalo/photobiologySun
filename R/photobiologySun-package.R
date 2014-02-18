a92b331e721e89b9eced056815a09b4f96b5080c
Pedro J. Aphalo <pedro.aphalo@helsinki.fi>
2014-02-19 00:02:34 +0200

    First commit.

---
+++ R/photobiologySun-package.R
@@ -0,0 +1,36 @@
+#' @title Spectral data for sun
+#' 
+#' @description This package provides data for the sun. The package is designed 
+#' to complement package \code{photobiology}.
+#' 
+#' @docType package
+#' @keywords misc
+#' @name photobiologySun-package
+#' @author Pedro J. Aphalo
+#' @details
+#' \tabular{ll}{
+#' Package: \tab photobiologySun\cr
+#' Type: \tab Package\cr
+#' Version: \tab 0.1.0\cr
+#' Date: \tab 2014-02-18\cr
+#' License: \tab GPL (>= 3)\cr
+#' URL: \tab \url{http://uv4growth.dyndns.org},\cr \tab \url{http://openinstruments.dyndns.org},\cr
+#' \tab \url{https://bitbucket.org/aphalo/photobiology}\cr
+#' BugReports: \tab \url{https://bitbucket.org/aphalo/photobiology}\cr
+#' }
+#' 
+#' @references
+#' Aphalo, P. J., Albert, A., Björn, L. O., McLeod, A. R., Robson, T. M., 
+#' Rosenqvist, E. (Eds.). (2012). Beyond the Visible: A handbook of best 
+#' practice in plant UV photobiology (1st ed., p. xxx + 174). 
+#' Helsinki: University of Helsinki, Department of Biosciences, 
+#' Division of Plant Biology. ISBN 978-952-10-8363-1 (PDF), 
+#' 978-952-10-8362-4 (paperback). Open access PDF download available at 
+#' \url{http://hdl.handle.net/10138/37558}
+#' 
+#' @examples
+#' library(photobiologySun)
+#' library(photobiologyVIS)
+#' with(sun.data, photon_ratio(w.length, s.e.irrad, Blue(), Green()))
+#' plot(s.e.irrad~w.length, data=sun1.data, main=comment(sun1.data), type="l")
+NULL
