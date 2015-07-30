72ad87091607107b2d3d191b4d3fd531e43204a7
Pedro J. Aphalo <pedro.aphalo@helsinki.fi>
2015-07-30 13:30:16 +0300

    Rebuild data objects and make vignettes not dependent on data.table. Heavy editing of data building scripts.

--- data-raw/build.hourly.data2014.r
+++ data-raw/build.hourly.data2014.r
@@ -1,36 +1,32 @@
 library(lubridate)
-library(data.table)
 library(photobiology)
+library(dplyr)
+
 first.day.spct <- read.table(file="data-raw/2014-08-21.hel", col.names = c("time", "w.length", "s.e.irrad"))
-setDT(first.day.spct)
-first.day.spct[ , UTC := ymd("2014-08-21") + seconds(trunc(time * 60))]
+first.day.spct[["UTC"]] <-  with(first.day.spct, ymd("2014-08-21") + seconds(trunc(time * 60)))
 
 second.day.spct <- read.table(file="data-raw/2014-08-22.hel", col.names = c("time", "w.length", "s.e.irrad"))
-setDT(second.day.spct)
-second.day.spct[ , UTC := ymd("2014-08-22") + seconds(trunc(time * 60))]
+second.day.spct[["UTC"]] <-  with(second.day.spct, ymd("2014-08-22") + seconds(trunc(time * 60)))
 
-hourly.spct <- rbindlist(list(first.day.spct, second.day.spct))
-hourly.spct[ , time := NULL]
-hourly.spct[ , w.length := w.length / 10]
-hourly.spct[ , s.e.irrad := s.e.irrad * 1e-3]
-
+hourly.spct <- rbind(tbl_df(first.day.spct), tbl_df(second.day.spct))
+hourly.spct <- transmute(hourly.spct, 
+                         w.length = w.length * 1e-1,
+                         s.e.irrad = s.e.irrad * 1e-3, 
+                         UTC = UTC)
 # The data set has no data for nights, so to make sure
 # that this is clear to users, and to ensure that plotted
 # lines get interrupted during nights, we insert NAs
-# at the begging and end of the night.
+# at the start and end of the night.
 # 
-night21.spct <- data.table(w.length = 290:800, 
+night21.spct <- data_frame(w.length = 290:800,
                             s.e.irrad = NA, 
                             UTC = ymd_hm("2014-08-21 19:22"))
-night22.spct <- data.table(w.length = 290:800, 
+night22.spct <- data_frame(w.length = 290:800, 
                             s.e.irrad = NA, 
                             UTC = ymd_hm("2014-08-22 1:22"))
-sun_hourly_august.spct <- rbindlist(list(hourly.spct,
-                                  night21.spct,
-                                  night22.spct))
+sun_hourly_august.spct <- rbind(hourly.spct, night21.spct, night22.spct)
+sun_hourly_august.spct <- mutate(sun_hourly_august.spct, EEST = with_tz(UTC, "EET"))
 
-sun_hourly_august.spct[ , EEST :=  with_tz(UTC, "EET")]
 sun_hourly_august.spct <- setSourceSpct(sun_hourly_august.spct, multiple.wl = 31)
-setkey(sun_hourly_august.spct, UTC, w.length)
 
 devtools::use_data(sun_hourly_august.spct, overwrite = TRUE)
